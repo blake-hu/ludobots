8608425520955f4916a581b506f7b2da2c42b80e
Blake Hu <64202537+blake-hu@users.noreply.github.com>
2022-12-28 09:54:22 +0800

    Create simulation time steps

--- simulate.py
+++ simulate.py
@@ -1,1 +1,10 @@
-pass
+import pybullet as p
+import time as time
+
+physicsClient = p.connect(p.GUI)
+for i in range(1000):
+	p.stepSimulation()
+	print("step: " + str(i))
+	time.sleep(0.01)
+
+p.disconnect()
