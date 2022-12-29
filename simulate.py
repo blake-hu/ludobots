e3c5fa5ccbbaef47a1e085d957ca2d59e3544ab2
Blake Hu <64202537+blake-hu@users.noreply.github.com>
2022-12-29 09:40:08 +0800

    Create manylinks in 5x5 grid

--- simulate.py
+++ simulate.py
@@ -1,10 +1,17 @@
 import pybullet as p
+import pybullet_data
 import time as time
 
 physicsClient = p.connect(p.GUI)
-for i in range(1000):
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
+
+p.setGravity(0, 0, -9.8)
+
+for i in range(10000):
 	p.stepSimulation()
 	print("step: " + str(i))
-	time.sleep(0.01)
+	time.sleep(0.001)
 
 p.disconnect()
