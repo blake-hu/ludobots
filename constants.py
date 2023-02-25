5ed4dab5c6770920fc9edec29902db57db1c3f58
Blake Hu <64202537+blake-hu@users.noreply.github.com>
2023-02-25 17:59:51 -0600

    Create empty classes

---
+++ constants.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# For simulation
+duration = 10  # Simulation duration in seconds
+steps = duration * 100
+
+# For motor control
+num_gridpoints = 100
+gridpoints = np.linspace(0, 2*np.pi, num_gridpoints)
+frontLegCommand = np.sin(gridpoints)*np.pi/4
+backLegCommand = np.sin(gridpoints+np.pi/4)*np.pi/4
+# plt.plot(frontLegCommand)
+# plt.plot(backLegCommand)
+# plt.show()
+# exit()
+
+gravity = -9.8
--- simulate.py
+++ simulate.py
@@ -6,60 +6,57 @@ import time as time
 import random
 # import matplotlib.pyplot as plt
 
+import constants as c
+from simulation import SIMULATION
+
+
+simulation = SIMULATION()
+
 
 def simulate():
-    physicsClient = pyb.connect(pyb.GUI)
-    pyb.setAdditionalSearchPath(pybullet_data.getDataPath())
-    planeId = pyb.loadURDF("plane.urdf")
-    robotId = pyb.loadURDF("body.urdf")
-    pyb.loadSDF("world.sdf")
+    pass
+    # physicsClient = pyb.connect(pyb.GUI)
+    # pyb.setAdditionalSearchPath(pybullet_data.getDataPath())
+    # planeId = pyb.loadURDF("plane.urdf")
+    # robotId = pyb.loadURDF("body.urdf")
+    # pyb.loadSDF("world.sdf")
 
-    pyb.setGravity(0, 0, -9.8)
+    # pyb.setGravity(0, 0, c.gravity)
 
-    duration = 10  # Simulation duration in seconds
-    steps = duration * 100
-    print("Simulating for " + str(duration) + " seconds")
+    # print("Simulating for " + str(c.duration) + " seconds")
 
-    pyrosim.Prepare_To_Simulate(robotId)
-    frontLegSensorValues = np.zeros(steps)
-    backLegSensorValues = np.zeros(steps)
+    # pyrosim.Prepare_To_Simulate(robotId)
+    # frontLegSensorValues = np.zeros(c.steps)
+    # backLegSensorValues = np.zeros(c.steps)
 
-    gridpoints = np.linspace(0, 2*np.pi, 100)
-    frontLegCommand = np.sin(gridpoints)*np.pi/4
-    backLegCommand = np.sin(gridpoints+np.pi/4)*np.pi/4
-    # plt.plot(frontLegCommand)
-    # plt.plot(backLegCommand)
-    # plt.show()
-    # exit()
+    # for i in range(c.steps):
+    #     pyb.stepSimulation()
+    #     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link(
+    #         "FrontLeg")
+    #     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link(
+    #         "BackLeg")
+    #     pyrosim.Set_Motor_For_Joint(
+    #         bodyIndex=robotId,
+    #         jointName="Torso_BackLeg",
+    #         controlMode=pyb.POSITION_CONTROL,
+    #         targetPosition=c.frontLegCommand[i % c.num_gridpoints],
+    #         maxForce=50,
+    #     )
+    #     pyrosim.Set_Motor_For_Joint(
+    #         bodyIndex=robotId,
+    #         jointName="Torso_FrontLeg",
+    #         controlMode=pyb.POSITION_CONTROL,
+    #         targetPosition=c.backLegCommand[i % c.num_gridpoints],
+    #         maxForce=50,
+    #     )
 
-    for i in range(steps):
-        pyb.stepSimulation()
-        frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link(
-            "FrontLeg")
-        backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link(
-            "BackLeg")
-        pyrosim.Set_Motor_For_Joint(
-            bodyIndex=robotId,
-            jointName="Torso_BackLeg",
-            controlMode=pyb.POSITION_CONTROL,
-            targetPosition=frontLegCommand[i % len(gridpoints)],
-            maxForce=50,
-        )
-        pyrosim.Set_Motor_For_Joint(
-            bodyIndex=robotId,
-            jointName="Torso_FrontLeg",
-            controlMode=pyb.POSITION_CONTROL,
-            targetPosition=backLegCommand[i % len(gridpoints)],
-            maxForce=50,
-        )
+    #     if i % 100 == 0:
+    #         print("Time elapsed: " + str(i/100) + "s")
+    #     time.sleep(0.001)
 
-        if i % 100 == 0:
-            print("Time elapsed: " + str(i/100) + "s")
-        time.sleep(0.001)
-
-    np.save("./data/frontLegSensorValues.npy", frontLegSensorValues)
-    np.save("./data/backLegSensorValues.npy", backLegSensorValues)
-    pyb.disconnect()
+    # np.save("./data/frontLegSensorValues.npy", frontLegSensorValues)
+    # np.save("./data/backLegSensorValues.npy", backLegSensorValues)
+    # pyb.disconnect()
 
 
 if __name__ == "__main__":
